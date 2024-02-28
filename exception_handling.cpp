5f8eba0db034def6292d717c9bebb341872c0036
Adarsh M <adarshmudamparambath@gmail.com>
2024-02-28 13:04:10 +0530

    reduce no of line

--- exception_handling.cpp
+++ exception_handling.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     try {
-        // Attempt to execute code that may throw an exception
+       
         int numerator, denominator;
         double result;
 
@@ -14,22 +14,20 @@ int main() {
         cout << "Enter the denominator: ";
         cin >> denominator;
 
-        // Attempt division operation within the try block
+       
         try {
             if (denominator == 0) {
-                throw "Division by zero is not allowed"; // Throwing a C-style string
+                throw "Division by zero is not allowed"; 
             }
 
             result = static_cast<double>(numerator) / denominator;
             cout << "Result of division: " << result << endl;
         }
         catch (const char* error) {
-            // Catch and handle the division by zero exception
             cerr << "Error: " << error << endl;
         }
     }
     catch (...) {
-        // Catch any other unexpected exceptions
         cerr << "An unexpected error occurred" << endl;
     }
 
